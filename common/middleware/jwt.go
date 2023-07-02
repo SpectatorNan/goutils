0e49c7ce8969d6c1688893d3be37e517f3eed3bc
spectatorMrZ <zhuchunan001@sina.com>
2023-07-02 17:54:58 +0800

    fix  jwt fetch user id failed

--- common/middleware/jwt.go
+++ common/middleware/jwt.go
@@ -8,11 +8,11 @@ import (
 
 const (
 	//JwtCustomClaimKey = "spectatornan:goutils:jwt:customClaim"
-	JwtBaseClaimKey   = "spectatornan:goutils:jwt:baseClaim"
+	JwtBaseClaimKey = "spectatornan:goutils:jwt:baseClaim"
 )
 
 func FetchUserIdByJwt[T jwtx.BaseClaim](ctx context.Context) (int64, error) {
-	u, ok := ctx.Value(JwtBaseClaimKey).(T)
+	u, ok := ctx.Value(JwtBaseClaimKey).(jwtx.CustomClaims[T])
 	if !ok {
 		logx.WithContext(ctx).Infof("【valid auth token】: User JWT token illegal")
 		return 0, loginExpireErr //errorc.LoginExpireErrCode //errorx.NewMsgCodeError(errorx.UnLoginCode, "请重新登录")
