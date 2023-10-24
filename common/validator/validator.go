c46b05c22e8e60e1466facce0039f1bc464eb195
spectatorMrZ <zhuchunan001@sina.com>
2023-10-24 11:28:43 +0800

    feat: add zero http validator

--- common/validator/validator.go
+++ common/validator/validator.go
@@ -1,8 +1,10 @@
 package validator
 
 import (
+	"github.com/SpectatorNan/goutils/common/errorx"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	"net/http"
 )
 
 type Validator struct {
@@ -12,4 +14,22 @@ type Validator struct {
 
 func (v Validator) Valid(s interface{}) (string, bool) {
 	return v.valid(s)
+}
+
+func (v Validator) Validate(r *http.Request, data any) error {
+	//val, ok := data.(*UserLoginReq)
+	//if !ok {
+	//	return errors.New("data is not correct type")
+	//}
+	//
+	//if *val == req {
+	//	return nil
+	//}
+	validate := r.Context().Value(I18nKey).(*Validator)
+	msg, valid := validate.Valid(data)
+	if !valid {
+		//respx.HttpResult(r, w, nil, errorx.NewErrMsg(msg))
+		return errorx.NewErrMsg(msg)
+	}
+	return nil
 }
