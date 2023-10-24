bd7e3f183661ff392e11a8eb383b8dd244a6c35a
spectatorMrZ <zhuchunan001@sina.com>
2023-10-24 11:48:34 +0800

    feat: add unified redis config portal

---
+++ common/cachex/redis_conf.go
@@ -0,0 +1,53 @@
+package cachex
+
+import (
+	goredis "github.com/redis/go-redis/v9"
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"time"
+)
+
+type RedisSingleNodeConf struct {
+	Host     string
+	Pass     string `json:",optional"`
+	Tls      bool   `json:",optional"`
+	NonBlock bool   `json:",default=true"`
+	// PingTimeout is the timeout for ping redis.
+	PingTimeout time.Duration `json:",default=1s"`
+}
+
+func (conf RedisSingleNodeConf) ToG0RedisConf() redis.RedisConf {
+	return redis.RedisConf{
+		Host:        conf.Host,
+		Type:        "node",
+		Pass:        conf.Pass,
+		Tls:         conf.Tls,
+		NonBlock:    conf.NonBlock,
+		PingTimeout: conf.PingTimeout,
+	}
+}
+
+func (conf RedisSingleNodeConf) ToG0CacheConf() cache.CacheConf {
+	return []cache.NodeConf{
+		{
+			RedisConf: redis.RedisConf{
+				Host:        conf.Host,
+				Type:        "node",
+				Pass:        conf.Pass,
+				Tls:         conf.Tls,
+				NonBlock:    conf.NonBlock,
+				PingTimeout: conf.PingTimeout,
+			},
+			Weight: 100,
+		},
+	}
+}
+
+func (conf RedisSingleNodeConf) ToGoRedis() *goredis.Client {
+	rdb := goredis.NewClient(&goredis.Options{
+		Addr:     conf.Host,
+		Password: conf.Pass,
+		DB:       0,
+	})
+	return rdb
+}
