d0d806e21e4c46126e9b5c1416a8b909044ddb42
spectatorMrZ <zhuchunan001@sina.com>
2023-10-24 11:43:12 +0800

    feat: add GoZeroValidator

--- common/validator/validator.go
+++ common/validator/validator.go
@@ -16,19 +16,12 @@ func (v Validator) Valid(s interface{}) (string, bool) {
 	return v.valid(s)
 }
 
-func (v Validator) Validate(r *http.Request, data any) error {
-	//val, ok := data.(*UserLoginReq)
-	//if !ok {
-	//	return errors.New("data is not correct type")
-	//}
-	//
-	//if *val == req {
-	//	return nil
-	//}
+type GoZeroValidator struct{}
+
+func (v GoZeroValidator) Validate(r *http.Request, data any) error {
 	validate := r.Context().Value(I18nKey).(*Validator)
 	msg, valid := validate.Valid(data)
 	if !valid {
-		//respx.HttpResult(r, w, nil, errorx.NewErrMsg(msg))
 		return errorx.NewErrMsg(msg)
 	}
 	return nil
