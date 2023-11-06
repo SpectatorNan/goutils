2b10f507462923efffafe45ad37743e2959762e7
spectatorMrZ <zhuchunan001@sina.com>
2023-11-06 15:27:49 +0800

    fix: captcha store default expire time overstack cause invalid

--- common/base64Captchax/store_redis_default.go
+++ common/base64Captchax/store_redis_default.go
@@ -3,12 +3,11 @@ package base64Captchax
 import (
 	"github.com/mojocn/base64Captcha"
 	"github.com/zeromicro/go-zero/core/stores/redis"
-	"time"
 )
 
 var (
-	Expiration        = 10 * time.Minute
+	Expiration        = 10 * 60 // 10 minutes
 	DefaultRedisStore = func(redis *redis.Redis, prefix string) base64Captcha.Store {
-		return NewRedisStore(redis, int(Expiration), prefix)
+		return NewRedisStore(redis, Expiration, prefix)
 	}
 )
