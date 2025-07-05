9cf1baf4336fb5da066845c359a2ab5758a13b09
spectatorMrZ <zhuchunan001@sina.com>
2025-07-05 22:29:00 +0800

    feat: route not found and not allow. standard response handlers

--- middleware/restx/route_handler.go
+++ middleware/restx/route_handler.go
@@ -1,24 +1,30 @@
 package restx
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/SpectatorNan/goutils/respx"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 	"time"
 )
 
+func RouteNotFoundWithNotAllow() []rest.RunOption {
+	return []rest.RunOption{
+		RouteNotFound(),
+		RouteMethodNotAllow(),
+	}
+}
+
 func RouteMethodNotAllow() rest.RunOption {
 	return rest.WithNotAllowedHandler(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			_, _ = w.Write(notAllowBytes(r))
+			httpx.WriteJson(w, http.StatusMethodNotAllowed, notAllowData(r))
 		}),
 	)
 }
 
-func notAllowBytes(r *http.Request) []byte {
+func notAllowData(r *http.Request) map[string]interface{} {
 	m := map[string]interface{}{
 		"code":      http.StatusMethodNotAllowed,
 		"timestamp": time.Now(),
@@ -27,6 +33,36 @@ func notAllowBytes(r *http.Request) []byte {
 		"message":   fmt.Sprintf("Method Not Allow, [%s]", r.Method),
 		"path":      r.URL.Path,
 	}
-	result, _ := json.Marshal(m)
-	return result
+	return m
+}
+
+func RouteNotFound() rest.RunOption {
+	return rest.WithNotFoundHandler(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			httpx.WriteJson(w, http.StatusNotFound, notFoundData(r))
+		}),
+	)
+}
+func notFoundData(r *http.Request) map[string]interface{} {
+	m := map[string]interface{}{
+		"code":      http.StatusNotFound,
+		"timestamp": time.Now(),
+		"status":    http.StatusNotFound,
+		"error":     "Route Not Found",
+		"message":   "The requested resource was not found",
+		"path":      r.URL.Path,
+	}
+	return m
+}
+
+func WithHealthCheckApiHandler() rest.RunOption {
+	return func(server *rest.Server) {
+		server.AddRoute(rest.Route{
+			Method: http.MethodGet,
+			Path:   "/healthcheck",
+			Handler: func(w http.ResponseWriter, r *http.Request) {
+				respx.HttpResult(r, w, map[string]string{"status": "ok"}, nil)
+			},
+		})
+	}
 }
